88fa8a147a02eb161463e6f6808c2e0225d32e0a
Vladislav Gapurov <gapurov@gmail.com>
2023-01-12 22:33:42 +0400

    init

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dr. Henning Hermes';
+export const SITE_DESCRIPTION = 'I am a Postdoctoral Researcher at the Düsseldorf Institute for Competition Economics (DICE) at HHU Düsseldorf. In addition, I am affiliated with the FAIR Center of Excellence, an IZA Research Affiliate, a CEPR Research Affiliate, a CESifo Research Network Affiliate, and a Research Fellow at CIDER.';
