dc6c90a779fd36d1cfe5af7e65761c2c2e46f598
Vladislav Gapurov <gapurov@gmail.com>
2023-04-19 12:53:10 +0200

    Add sitemap tag in head

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://henninghermes.com/',
+  site: 'https://www.henninghermes.com/',
   integrations: [
     mdx(),
     sitemap(),
